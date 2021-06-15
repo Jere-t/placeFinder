37a38477c8e822693da8a657e9d495b785e2f947
Adorax <jeremy.maret@gmail.com>
2021-06-15 17:49:46 +0200

    feat(add-files): add datasets, mock-up, test files for google place api

---
+++ google-place-api/reactScript.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const e = React.createElement;
+
+// https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=46.5196535,6.6322734&radius=100&type=university&key=xxx -->
+// location, radius, type, keyword, minprice, maxprice, rankby, opennow, ...
+// API keys: in secret File (cannot be commited)
+
